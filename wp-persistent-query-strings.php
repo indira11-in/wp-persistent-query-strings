2e081d13396b2fa64d03a64f4853f9a841cc5995
Joey Scarim <joeyscarim@gmail.com>
2019-02-15 15:04:12 -0700

    add default settings screen

--- wp-persistent-query-strings.php
+++ wp-persistent-query-strings.php
@@ -95,3 +95,34 @@ add_filter('home_url', 'add_my_query_var');
 add_filter('network_site_url', 'add_my_query_var');
 add_filter('network_home_url', 'add_my_query_var');
 add_filter('network_admin_url', 'add_my_query_var');
+
+add_action('admin_menu', 'plugin_admin_add_page');
+function plugin_admin_add_page()
+{
+    add_options_page('Custom Plugin Page', 'WP Persistent Query Strings', 'manage_options', 'plugin', 'plugin_options_page');
+}
+
+function plugin_options_page()
+{
+    ?>
+<div>
+<h2>WP Persistent Query Strings</h2>
+These settings are coming soon.
+<form action="options.php" method="post">
+<?php settings_fields('plugin_options');?>
+<?php do_settings_sections('plugin');?>
+
+<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes');?>" />
+</form></div>
+
+<?php
+}?>
+
+<?php
+add_action('admin_init', 'plugin_admin_init');
+function plugin_admin_init()
+{
+    register_setting('plugin_options', 'plugin_options', 'plugin_options_validate');
+    add_settings_section('plugin_main', 'Main Settings', 'plugin_section_text', 'plugin');
+    add_settings_field('plugin_text_string', 'Plugin Text Input', 'plugin_setting_string', 'plugin', 'plugin_main');
+}
